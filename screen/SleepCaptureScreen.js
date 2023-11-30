88455df5d1fbc0df572e6b90cc430f20de5627da
geonu <rjsdndl909@gmail.com>
2023-11-30 13:40:56 +0900

    test complete

--- screen/SleepCaptureScreen.js
+++ screen/SleepCaptureScreen.js
@@ -1,11 +1,150 @@
-import React from "react";
-import { Text,View } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  StyleSheet,
+  Image,
+  Alert,
+  ActivityIndicator,
+} from "react-native";
+import { Audio } from "expo-av";
+import axios from "axios";
+import io from "socket.io-client";
 
+// 서버주소 변경
+const SERVER_URL = "http://210.119.103.114:5000";
 
-export default function SleepCapture() {
-  return(
-    <View>
-      <Text>test Sleep</Text>
+export default function App() {
+  const [drowsinessImage, setDrowsinessImage] = useState(null);
+  const [imageKey, setImageKey] = useState(0);
+  const [sound, setSound] = useState(false);
+  const socket = io(SERVER_URL); // Flask 서버 URL로 교체하세요
+
+  // 웹소켓 커넥트
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("서버에 연결됨");
+    });
+
+    socket.on("alarm_played", (data) => {
+      console.log("알람이 울렸음:", data);
+      // React Native 측에서 원하는 동작을 트리거합니다.
+      // 예를 들어 play_alarm_function 호출
+      play_alarm_function();
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  //초기 로딩 및 cv 시작
+  useEffect(() => {
+    projectStart();
+    reloadImage();
+  }, []);
+
+  // 사운드 및 경고 알림 구현 코드 시작부분
+  async function playSound() {
+    console.log("사운드 로딩 중");
+    const { sound } = await Audio.Sound.createAsync(
+      require("../assets/war.wav")
+    );
+    setSound(sound);
+
+    console.log("사운드 재생 중");
+    await sound.playAsync();
+  }
+  const showAlertDialog = async () => {
+    try {
+      Alert.alert(
+        "졸음 알림",
+        "졸음운전이 예상됩니다. 쉬어가세요!",
+        [
+          {
+            text: "확인",
+            onPress: async () => {
+              // 사운드 중지
+              if (sound) {
+                try {
+                  await sound.stopAsync();
+                  await sound.unloadAsync();
+                  console.log("확인 버튼이 눌렸습니다.");
+                } catch (error) {
+                  // console.error("사운드 중지 또는 언로드 중 오류:", error);
+                }
+              }
+              // else {
+              //   console.warn("사운드가 로드되지 않았습니다.");
+              // }
+            },
+          },
+        ],
+        { cancelable: false }
+      );
+      // 이미지 불러오기
+      reloadImage();
+    } catch (error) {
+      console.error("사운드 재생 중 오류:", error);
+    }
+  };
+  // 사운드 및 경고 알림 구현 코드 끝
+  useEffect(() => {
+    return sound
+      ? () => {
+          console.log("사운드 언로딩 중");
+          sound.unloadAsync();
+        }
+      : undefined;
+  }, [sound]);
+
+  // 서버에서 오는 요청 받는 코드
+  const play_alarm_function = async () => {
+    console.log("React Native 측에서 알람 재생 중");
+    await playSound();
+    showAlertDialog();
+  };
+
+  const reloadImage = async () => {
+    try {
+      const response = await axios.get(`${SERVER_URL}/sleep_image?${imageKey}`);
+      setDrowsinessImage(response.data.image);
+      setImageKey((prevKey) => prevKey + 1);
+    } catch (error) {
+      console.error("졸음 이미지를 불러오는 중 오류 발생:", error);
+    }
+  };
+
+  const projectStart = () => {
+    fetch(`${SERVER_URL}/video_feed`)
+      .then((response) => response.text())
+      .then((data) => console.log(data));
+    // .catch(error => console.error('Error:', error));
+  };
+
+  return (
+    <View style={styles.container}>
+      {drowsinessImage ? (
+        <Image
+          key={imageKey}
+          source={{ uri: `data:image/jpeg;base64,${drowsinessImage}` }}
+          style={styles.image}
+        />
+      ) : (
+        <ActivityIndicator size="large" color="#0000ff" />
+      )}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image: {
+    width: 200,
+    height: 200,
+    resizeMode: "contain",
+  },
+});
